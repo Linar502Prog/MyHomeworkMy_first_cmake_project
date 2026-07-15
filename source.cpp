c207632486f329e9ce716b24d17ec849aeeae9ee
Linar502Prog <LinarSSuleymanov@yandex.ru>
2026-07-15 22:35:45 +0400

    Добавьте файлы проекта.

---
+++ source.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+int main() {
+	setlocale(LC_ALL, "RUS");
+	std::string name{};
+	std::cout << "Введите имя : ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << std::endl;
+	return EXIT_SUCCESS;
+}
